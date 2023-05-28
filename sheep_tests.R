d74421e3c5eefd258de147003a967a3c022141d0
luisfueyo <101431300+luisfueyo@users.noreply.github.com>
2023-05-28 11:06:37 +0200

    Update sheep_tests.R

--- sheep_tests.R
+++ sheep_tests.R
@@ -1,5 +1,5 @@
 # 0. Preparation ---- 
-library(tidyverse)
+library(tidyverse, stargazer)
 
 # 1. Importing the data and cleaning ----
 ## We import the data
@@ -10,7 +10,7 @@ df <- df |>
   mutate_at(vars(ends_with("_id")), ~as_factor(.))
 
 # 2. Data wrangling ----
-## We take the mean for each group for comparing between initial conditions
+## We take the mean for each group according to the initial condition
 condition <- df |> 
   group_by(condition_id) |> 
   summarise(tail = mean(tail_presence)) |> 
@@ -26,13 +26,23 @@ condition_2 <- df |>
   filter(condition_id == 2) |> 
   select(tail_presence)
 
+## We make the one-sided t-test
 t.test(condition_2, condition_1, alternative = "greater")
 
+## 3.2 We run a probit regression to see how demographic factors affect the drawings
+sheep_probit <- glm(tail_presence ~ sex + age_group + as_factor(condition_id), 
+                  family = binomial(link = "probit"), 
+                  data = df)
+
+summary(sheep_probit)
+
 # 4. Data visualization ---
+## Graph the difference between groups
 initial_condition_graph <- condition |> 
   ggplot(aes(condition_id, tail)) +
   geom_col() +
   theme_minimal() +
   labs(x= "Condition", y="Mean tail presence")
 
+## Save the plot
 #ggsave("initial_condition_graph.png", initial_condition_graph)
